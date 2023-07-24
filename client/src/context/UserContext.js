d2c899fefb19d90017d6f716334a6bcf2f4d045d
Andrew Wang <Andrewruujin@gmail.com>
2023-07-23 23:40:24 -0400

    have sessions working

--- client/src/context/UserContext.js
+++ client/src/context/UserContext.js
@@ -7,14 +7,14 @@ const UserContext = React.createContext();
 function UserProvider({ children }) {
   const [user, setUser] = useState(null);
   
-  // useEffect(() => {
-  //   fetch("/me")
-  //   .then((r) => {
-  //     if (r.ok) {
-  //       r.json().then((user) => setUser(user));
-  //     }
-  //   });
-  // }, []);
+  useEffect(() => {
+    fetch("/me")
+    .then((r) => {
+      if (r.ok) {
+        r.json().then((user) => setUser(user));
+      }
+    });
+  }, []);
 
   return <UserContext.Provider value={{user, setUser}}>{children}</UserContext.Provider>;
 }
