ba63db7463f52e53f55c7efa946f9f548a15a6ee
Andrew Wang <Andrewruujin@gmail.com>
2023-08-14 16:11:18 -0400

    add total orders

--- app/serializers/product_serializer.rb
+++ app/serializers/product_serializer.rb
@@ -4,10 +4,13 @@ class ProductSerializer < ActiveModel::Serializer
   def product_with_users
     unique_users = object.users.uniq {|user| user.id}
     unique_users.map do |user|
+      order_quantity = user.user_products.filter {|order| order.product_id == object.id}
+      total_quantity = order_quantity.sum(&:quantity)
         {
           user_id: user.id,
           user_name: user.name,
-          user_fund: user.fund
+          user_fund: user.fund,
+          total_quantity: total_quantity
         }
       
     end
--- client/src/components/UserOrderCard.js
+++ client/src/components/UserOrderCard.js
@@ -1,12 +1,13 @@
 
 export default function UserOrderCard({userOrder}){
-    const {user_name, user_fund} = userOrder
+    const {user_name, user_fund, total_quantity} = userOrder
 
     return(
         <div className="card">
             
             <h2>Name: {user_name}</h2>
             <p>Funds: ${user_fund}</p>
+            <p>ordered: {total_quantity}</p>
 
         </div>
     )
