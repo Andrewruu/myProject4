76f40cec85af5460466b4185fc09cce3737cf564
Andrew Wang <Andrewruujin@gmail.com>
2023-08-14 15:45:14 -0400

    remove console log

--- client/src/page/UserOrders.js
+++ client/src/page/UserOrders.js
@@ -6,15 +6,11 @@ import { useParams } from "react-router-dom";
 function UserOrders(){
     const {products} = useContext(ProductContext)
     const {id} = useParams()
-    console.log("product is")
-    console.log(products)
     const product = products.find( p => p.id === parseInt(id))
     if (!product){
         return<h1>Product not found!</h1>
     }
-    console.log(product)
     const userOrders = product.product_with_users
-    console.log(product.product_with_users)
 
     const userOrdersList = (userOrders.length>0? (
         <div>{
